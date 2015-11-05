3d6d7d412c60f362c8bb64981cd72ddd54c7950c
Michael Pratt <pratt@hablarmierda.net>
2015-11-05 17:08:08 -0500

    Version Bump

--- Lib/RelativeTime/RelativeTime.php
+++ Lib/RelativeTime/RelativeTime.php
@@ -23,7 +23,7 @@ use \DateInterval;
 class RelativeTime
 {
     /** @var int Class constant with the current Version of this library */
-    const VERSION = '1.5.1';
+    const VERSION = '1.5.3';
 
     /** @var array Array With configuration options **/
     protected $config = array();
