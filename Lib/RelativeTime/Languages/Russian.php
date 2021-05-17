5eb8aca871e78b6de9a90a1b5fe06553e089b6e7
Vladimir Barinov <veselcraft@icloud.com>
2021-05-17 17:58:02 +0300

    Create Russian.php

---
+++ Lib/RelativeTime/Languages/Russian.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Russian.php
+ *
+ * @author  Vladimír Barinov <veselcraft@yandex.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+namespace RelativeTime\Languages;
+
+/**
+ * Russian Translation
+ */
+class Russian extends LanguageAdapter
+{
+    protected $strings = array(
+        'now' => 'только что',
+        'ago' => '%s назад',
+        'left' => 'осталось %s',
+        'seconds' => array(
+            'plural' => '%d секунд',
+            'singular' => '%d секунда',
+        ),
+        'minutes' => array(
+            'plural' => '%d минут',
+            'singular' => '%d минута',
+        ),
+        'hours' => array(
+            'plural' => '%d часов',
+            'singular' => '%d час',
+        ),
+        'days' => array(
+            'plural' => '%d дней',
+            'singular' => '%d день',
+        ),
+        'months' => array(
+            'plural' => '%d месяцев',
+            'singular' => '%d месяц',
+        ),
+        'years' => array(
+            'plural' => '%d лет',
+            'singular' => '%d год',
+        ),
+    );
+}
