976db7deed8efef2b5958947711eeabd2d616fea
Michael Pratt <commits@michael-pratt.com>
2023-06-02 21:15:10 -0500

    Released Version 1.5.13

--- Lib/RelativeTime/RelativeTime.php
+++ Lib/RelativeTime/RelativeTime.php
@@ -23,7 +23,7 @@ use \DateInterval;
 class RelativeTime
 {
     /** @const int Class constant with the current Version of this library */
-    const VERSION = '1.5.12';
+    const VERSION = '1.5.13';
 
     /** @var array Array With configuration options **/
     protected $config = array();
